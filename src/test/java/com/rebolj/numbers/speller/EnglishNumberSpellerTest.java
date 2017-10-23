52fd194b9d4c865f859091c4b946e848de506d6f
Gorazd Rebolj <gorazdrebolj>
2017-10-23 15:54:20 +0100

    Remove code duplication in EnglishNumberSpellerTest

--- src/test/java/com/rebolj/numbers/speller/EnglishNumberSpellerTest.java
+++ src/test/java/com/rebolj/numbers/speller/EnglishNumberSpellerTest.java
@@ -1,5 +1,6 @@
 package com.rebolj.numbers.speller;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
@@ -10,23 +11,27 @@ import java.util.stream.Collectors;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class EnglishNumberSpellerTest {
+    private EnglishNumberSpeller numberSpeller;
+
+    @BeforeEach
+    void setUp() {
+        numberSpeller = new EnglishNumberSpeller();
+    }
+
     @ParameterizedTest(name = "{0} should spell {1}")
     @CsvFileSource(resources = "/test-samples.csv")
     void spell(long value, String spelling) {
-        final NumberSpeller numberSpeller = new EnglishNumberSpeller();
         assertEquals(spelling, numberSpeller.spell(value));
     }
 
     @ParameterizedTest(name = "{0} should be split into {1}")
     @CsvSource({"1,1", "12,12", "123,123", "1234,1.234", "12345,12.345", "123456,123.456", "1234567,'1.234.567"})
     void toTriplets(int in, String out) {
-        final EnglishNumberSpeller numberSpeller = new EnglishNumberSpeller();
         assertEquals(out, numberSpeller.toTriplets(in).stream().map(String::valueOf).collect(Collectors.joining(".")));
     }
 
     @Test
     void appendSeparator() {
-        final EnglishNumberSpeller numberSpeller = new EnglishNumberSpeller();
         final StringBuilder builder = new StringBuilder();
 
         // don't append when no existing characters
@@ -41,7 +46,6 @@ class EnglishNumberSpellerTest {
 
     @Test
     void appendZero() {
-        final EnglishNumberSpeller numberSpeller = new EnglishNumberSpeller();
 
         // append when no existing characters
         StringBuilder builder = new StringBuilder();
